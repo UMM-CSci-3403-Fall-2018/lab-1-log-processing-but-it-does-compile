8b415fb2147add8993476ace81a7f72d727f66f9
stev0531 <stev0531@morris.umn.edu>
2018-09-11 10:42:43 -0500

    Augment process_client_logs.sh
    The script now selects only log entries
    that contain the string "Failed password"
    and changes to a more specific location to
    retreive the logs. All tests are now passing for
    this script now.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 directory="$1"
-cd $directory
 
-cat *| sed -r 's/ \<invalid user\>//g' | sed -r 's/:[0-9]+:[0-9]+//g' | awk '{print $1, $2, $3, $9, $11}' > test_log.txt
+cd $directory/var/log
+
+cat *|grep "Failed password"| sed -r 's/ \<invalid user\>//g' | sed -r 's/:[0-9]+:[0-9]+//g' | awk '{print $1, $2, $3, $9, $11}' > $directory/failed_login_data.txt
+
