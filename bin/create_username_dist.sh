57d82a2a4bcfc39dba0188a0684678ae837002a8
stev0531 <stev0531@morris.umn.edu>
2018-09-11 18:16:37 -0500

    Add awk commands to create_username script
    We've added commands to read data from
    failed_login_data.txt that extract the usernames,
    sorts and counts them, and wraps them in google
    charts syntax and stores them in a text file.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+top_directory=$1
+
+cd $top_directory
+
+cat failed_login_data.txt| awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > test.txt
+
+#bash /bin/wrap_contents.sh 
