622b420f4c7fcc7521a47f114bd5287fc98384f8
rabex028 <rabex028@morris.umn.edu>
2018-09-10 20:51:42 -0500

    Modify process_client_logs.sh
    Added an awk command for processing
    the client data that grabs the relevant
    columns from the output of the cat command
    and subsequent sed commands. Appears to produce
    the correct output, however the last test in the
    bat file for the correct output is still failing.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,4 +2,4 @@
 directory="$1"
 cd $directory
 
-cat *| sed -r 's/ \<invalid user\>//g' | sed -r 's/:[0-9]+:[0-9]+//g' > test_log.txt
+cat *| sed -r 's/ \<invalid user\>//g' | sed -r 's/:[0-9]+:[0-9]+//g' | awk '{print $1, $2, $3, $9, $11}' > test_log.txt
