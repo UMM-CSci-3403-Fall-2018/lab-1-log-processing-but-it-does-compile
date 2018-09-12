e48952e0cc8884f7fa17a5ad0393490406551611
stev0531 <stev0531@morris.umn.edu>
2018-09-11 19:31:59 -0500

    Make username_dist output html
    This script will now wrap the row data
    from failed_login_data.txt in the appropriate
    header and footer html for usernames, and will
    create a html file in the given directory.
    The last two tests are not passing however.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -3,7 +3,10 @@
 top_directory=$1
 
 cd $top_directory
-
+here=$(pwd)
+cd bin/test_usernames
 cat failed_login_data.txt| awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > test.txt
+cd $here
+bash bin/wrap_contents.sh bin/test_usernames/test.txt html_components/username_dist username_dist.html
 
-#bash /bin/wrap_contents.sh 
+rm bin/test_usernames/test.txt
