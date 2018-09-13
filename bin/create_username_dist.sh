8a32083c4c4e7df1161b7fda338e46886760bf10
rabex028 <rabex028@morris.umn.edu>
2018-09-12 19:10:15 -0500

    Make `create_username_dist.sh` behave
    It now goes through all the sub-directories to
    look for the file 'failed_login_data.txt'
    and wraps it in the proper addRow syntax and
    encases it in html. All tests passing.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-top_directory=$1
-
-cd $top_directory
+log_directory=$1
 here=$(pwd)
-cd bin/test_usernames
-cat failed_login_data.txt| awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > test.txt
-cd $here
-bash bin/wrap_contents.sh bin/test_usernames/test.txt html_components/username_dist username_dist.html
 
-rm bin/test_usernames/test.txt
+cd $log_directory 
+
+find -type f -name 'failed_login_data.txt' -exec cat {} >> temp.txt \;
+awk '{print $4}' temp.txt | sort | uniq -c \
+	| awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > temp2.txt
+
+rm temp.txt
+cd $here
+
+
+
+bash bin/wrap_contents.sh $log_directory/temp2.txt html_components/username_dist $log_directory/username_dist.html
+
+rm $log_directory/temp2.txt
+
+#rm $log_directory/test.txt
