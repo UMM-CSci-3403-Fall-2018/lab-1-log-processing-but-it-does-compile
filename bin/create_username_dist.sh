d96e2e73486cd5eba069293b24832ce365166f9b
rabex028 <rabex028@morris.umn.edu>
2018-09-12 19:27:15 -0500

    Add clean comments to scripts
    Spaced out the commands in our script
    files and added in-line documentation
    to aid clarity and cleanliness.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+#Colin Rabe and Elizabeth Stevens
+#University of Minnesota Morris
+#September 2018
+
 log_directory=$1
 here=$(pwd)
-
 cd $log_directory 
 
+# Recursively search the subdirectories for the file below and writes contents to temp
 find -type f -name 'failed_login_data.txt' -exec cat {} >> temp.txt \;
+
+# Prints the username column from temp, sorts and counts the resulting names
 awk '{print $4}' temp.txt | sort | uniq -c \
+
+	# Wraps the names and counts into google chart addRow syntax	
 	| awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > temp2.txt
 
 rm temp.txt
 cd $here
 
-
-
+# Calls wrap_contents on addRow data to be wrapped in the required html header and footer
 bash bin/wrap_contents.sh $log_directory/temp2.txt html_components/username_dist $log_directory/username_dist.html
 
 rm $log_directory/temp2.txt
-
-#rm $log_directory/test.txt
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#This script opens the given directory and
-#goes into the files var/log to access the
-#log files. Then searches all files for the
-#string "Failed password" and removes the string
-#"invalid user". After that it removes the 
-#digits after the colons and stores the wanted data
-#in the given directory into `failed_login_data.txt`
+# Colin Rabe and Elizabeth Stevens
+# University of Minnesota
+# September 2018
 
 directory="$1"
 
 cd $directory/var/log
 
-cat *|grep "Failed password"| sed -r 's/ \<invalid user\>//g' | sed -r 's/:[0-9]+:[0-9]+//g' | awk '{print $1, $2, $3, $9, $11}' > $directory/failed_login_data.txt
+# Reads log files, deletes 'invalid user' from lines that contain 'Failed password'
+cat *|grep "Failed password"| sed -r 's/ \<invalid user\>//g' \
+
+	# Removes Minutes and seconds from the time, prints the required columns
+	| sed -r 's/:[0-9]+:[0-9]+//g' | awk '{print $1, $2, $3, $9, $11}' \
+       
+	> $directory/failed_login_data.txt
 
