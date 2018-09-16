1e1b0a974caf7adecd2228cc5cd76e304ad21891
rabex028 <rabex028@morris.umn.edu>
2018-09-15 22:55:56 -0500

    Clean `create_username_dist.sh`
    Removed instances of creating manual
    temporary text files and replaced them with
    the unix implementation `mktemp` command to avoid
    overwriting pre-existing files in this script.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,22 +5,24 @@
 #September 2018
 
 log_directory=$1
+temp=$(mktemp)
+temp2=$(mktemp)
 here=$(pwd)
 cd $log_directory 
 
 # Recursively search the subdirectories for the file below and writes contents to temp
-find -type f -name 'failed_login_data.txt' -exec cat {} >> temp.txt \;
+find -type f -name 'failed_login_data.txt' -exec cat {} >> $temp \;
 
 # Prints the username column from temp, sorts and counts the resulting names
-awk '{print $4}' temp.txt | sort | uniq -c |
+awk '{print $4}' $temp | sort | uniq -c |
 
 	# Wraps the names and counts into google chart addRow syntax	
-	awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > temp2.txt
+	awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > $temp2
 
-rm temp.txt
+rm $temp
 cd $here
 
 # Calls wrap_contents on addRow data to be wrapped in the required html header and footer
-bash bin/wrap_contents.sh $log_directory/temp2.txt html_components/username_dist $log_directory/username_dist.html
+bash bin/wrap_contents.sh $temp2 html_components/username_dist $log_directory/username_dist.html
 
-rm $log_directory/temp2.txt
+rm $temp2
