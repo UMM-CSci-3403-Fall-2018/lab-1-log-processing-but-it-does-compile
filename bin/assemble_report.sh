9f5fc6501391ef9b3def4510dd4211e3bf2f58ea
rabex028 <rabex028@morris.umn.edu>
2018-09-15 22:33:26 -0500

    Alter `assemble_report.sh`
    Changed the ordering in which the html
    files to be found within the parameter
    directory for `assemble_report.sh` are
    concatenated together. As a result, all
    bats tests for this script are now
    passing.

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -9,7 +9,7 @@ temp=$(mktemp)
 here=$(pwd)
 cd $html_directory
 
-cat username_dist.html hours_dist.html country_dist.html > $temp
+cat country_dist.html hours_dist.html username_dist.html > $temp
 cd $here
 
 bash bin/wrap_contents.sh $temp  html_components/summary_plots $html_directory/failed_login_summary.html
