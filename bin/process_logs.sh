7a8dd158a2d72b058eec6dfc07377e7aa2814bc4
rabex028 <rabex028@morris.umn.edu>
2018-09-17 14:13:51 -0500

    Augment `process_logs.sh`
    This script now calls the scripts
    responsible for generating html on
    the failed login data and creates an
    html summary based on the compressed
    files it is given. Three of four tests
    for this script are passing.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,11 +1,21 @@
 #!bin/bash
 
 tempDir=$(mktemp -d)
+here=$(pwd)
 
-for var in "$@"
+for var in "$@";
 do
-	tar -xzf $var -C ${var%_*}
-	./bin/process_client_logs.sh ${var%_*}
+	mkdir -p $tempDir/${var%_*}
+	tar -zxvf $var -C $tempDir/${var%_*}
+	./bin/process_client_logs.sh $tempDir/${var%_*}
 done
+
+./bin/create_username_dist.sh $tempDir
+./bin/create_hours_dist.sh $tempDir
+./bin/create_country_dist.sh $tempDir
+./bin/assemble_report.sh $tempDir
+
+cd $here
+mv $tempDir/failed_login_summary.html $here
 
 rm -rf $tempDir
