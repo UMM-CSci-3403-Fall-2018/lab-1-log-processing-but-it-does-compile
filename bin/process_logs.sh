fe856df1206423456e2c6bf2dc37aa44659586f6
stev0531 <stev0531@morris.umn.edu>
2018-09-18 11:06:47 -0500

    Fix `process_logs.sh`
    Added a missing slash to the she-bang
    at the top of the script that was causing
    two tests to fail. Also included in-line
    comments within the script. All tests are
    now passing.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,21 +1,26 @@
-#!bin/bash
+#!/bin/bash
+
+#Elizabeth Stevens and Colin Rabe
+#University of Minnesota Morris
+#September 2018
 
 tempDir=$(mktemp -d)
 here=$(pwd)
 
+# Loops through all given files, untars them and generates failed login data for each
 for var in "$@";
 do
 	mkdir -p $tempDir/${var%_*}
-	tar -zxvf $var -C $tempDir/${var%_*}
+	tar -zxf $var -C $tempDir/${var%_*}
 	./bin/process_client_logs.sh $tempDir/${var%_*}
 done
 
+# Generates html files for each characteristic of the failed login data, assmebles them
 ./bin/create_username_dist.sh $tempDir
 ./bin/create_hours_dist.sh $tempDir
 ./bin/create_country_dist.sh $tempDir
 ./bin/assemble_report.sh $tempDir
 
-cd $here
+# Moves the summary html file out of the temporary directory
 mv $tempDir/failed_login_summary.html $here
-
 rm -rf $tempDir
