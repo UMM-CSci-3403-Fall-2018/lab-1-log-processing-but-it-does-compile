e1cee9b08829ea4d2b3e727d34eb1b76d4228a34
rabex028 <rabex028@morris.umn.edu>
2018-09-18 11:22:43 -0500

    Add comments to `assemble_report.sh`
    Wrote two lines of comments describing
    how the html files are processed.

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -9,9 +9,11 @@ temp=$(mktemp)
 here=$(pwd)
 cd $html_directory
 
+# Appends the content of the html files together
 cat country_dist.html hours_dist.html username_dist.html > $temp
 cd $here
 
+# Wraps their contents in summary header and footer
 bash bin/wrap_contents.sh $temp  html_components/summary_plots $html_directory/failed_login_summary.html
 
 rm $temp
